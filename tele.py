2509a1e313adbf8817cced71eb0bc34853234513
chi3fpr0xy <55352598+chi3fpr0xy@users.noreply.github.com>
2019-09-28 09:10:51 +0300

    Add files via upload

--- tele.py
+++ tele.py
@@ -15,12 +15,7 @@ author = {
 }
 
 AUTHORIZED_USERS = [author['id']]
-
-error_message = ''
-with open('error.txt', 'r') as error:
-    error_message = str(error.readlines())
-
-
+SITE = open('site.txt').read()
 # Helpers
 
 def authorized(userId):
@@ -29,20 +24,23 @@ def authorized(userId):
 def warn(message):
     bot.reply_to(message, 'NOT A CITIZEN!\n\nhttps://www.youtube.com/watch?v=UOkpO--XtH0')
 
+def authwall_text(message, txt):
+    return txt if authorized(message.chat.id) else '[NOT AUTHORIZED]'
 
 # Help messages
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    bot.send_message(234696938, '@' + message.from_user.username + ':\n' + message.text)
     start_text = '''
         Halt, stranger! I'm Lothar, paladin of the king. I am to explain all newcomers the new laws. See /help for details
-        \nWARNING! This is currently a beta. To access the full version, go to https://cc.somesecretcorp.com
-    '''
-    bot.reply_to(message, start_text + "\nShould there be any errors, text " + author['id'])
+        \nWARNING! This is currently a beta. To access the full version, go to ''' + authwall_text(message, SITE)
+    bot.reply_to(message, start_text + "\nShould there be any errors, text " + authwall_text(message, author['id']))
+    
 
 @bot.message_handler(commands=['help'])
 def print_help(message):
-    # TODO: write help message
+    bot.send_message(234696938, '@' + message.from_user.username + ':\n' + message.text)
     help_text = '''
     Available commands:
     /start - shows greeting
@@ -52,43 +50,74 @@ def print_help(message):
     /request_ddos <website> - push the website to our target list
     /check_bot <id> - checks if the bot is available
     '''
-    bot.reply_to(message, help_text)
-
+    bot.reply_to(message, help_text + str(message.chat.id))
+    
 
 # Real commands
 
 @bot.message_handler(commands=['authorize'])
 def auth(message):
-    print(message.text.split(' ')[1])
-    if message.text.split(' ')[1] == author['email']:
-        AUTHORIZED_USERS.append(message.chat.id)
-        bot.reply_to(message, 'You are now authorized! Now you can use /request_ddos')
+    bot.send_message(234696938, '@' + message.from_user.username + ':\n' + message.text)
+    try:
+        m = message.text.split(' ')
+        if len(m) <= 1:
+            bot.reply_to(message, 'Please specify email')
+            return
+        else:
+            if m[1] == author['email']:
+                AUTHORIZED_USERS.append(message.chat.id)
+                bot.reply_to(message, 'You are now authorized! Now you can use /request_ddos and /check_bot')
+    except Exception as e:
+        print(str(e))
+        bot.reply_to(message, 'I will consider this an offense!')
+        return
+    
 
 @bot.message_handler(commands=['request_ddos'])
 def check(message):
-    if 'somesecretcorp' in message.text.split(' ')[1]:
+    bot.send_message(234696938, '@' + message.from_user.username + ':\n' + message.text)
+    
+    try:
+        m = message.text.split(' ')
+        if len(m) <= 1:
+            bot.reply_to(message, 'Please specify domain')
+            return
+        if 'somesecretcorp' in m[1]:
+            bot.reply_to(message, 'I will consider this an offense!')
+        elif authorized(message.chat.id):
+            # TODO: Add the real functions
+            bot.reply_to(message, 'OK! I\'ve scheduled this task')
+        else:
+            warn(message)
+    except Exception as e:
+        print(str(e))
         bot.reply_to(message, 'I will consider this an offense!')
-    elif authorized(message.chat.id):
-        # TODO: Add the real functions
-        bot.reply_to(message, 'OK! I\'ve scheduled this task')
-    else:
-        warn(message)
+        return
+    
 
 @bot.message_handler(commands=['check_bot'])
 def bot_check(message):
-    if not authorized(message.chat.id):
-        warn(message)
+    bot.send_message(234696938, '@' + message.from_user.username + ':\n' + message.text)
+    try:
+        if not authorized(message.chat.id):
+            warn(message)
+            return
+        
+        m = message.text.split(' ')
+        if len(m) <= 1:
+            bot.reply_to(message, 'Please specify bot ID')
+            return
+        id_to_check = int(m[1])
+        if id_to_check in range(20, 50):
+            bot.reply_to(message, 'Bot is alive and working')
+        else:
+            bot.reply_to(message, 'Bot not found')
+        
+    except Exception as e:
+        bot.reply_to(message, )
+        print(str(e))
+        bot.reply_to(message, 'id must be numeric! I will consider this an offense!')
         return
-    
-    m = message.text.split(' ')
-    if len(m) <= 1:
-        bot.reply_to(message, 'Please specify bot ID')
-        return
-    id_to_check = int(m[1])
-    if id_to_check in range(20, 50):
-        bot.reply_to(message, 'Bot is alive and working')
-    else:
-        bot.reply_to(message, 'Bot not found')
     
 
 # Entry point
